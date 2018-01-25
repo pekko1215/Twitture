a2436ebb107b78f41f019ca094c81393efd47506
pekko1215 <pekko1215@yahoo.co.jp>
2018-01-25 16:46:45 +0900

    dwa

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({limit:'100mb',extended: true }));
 app.use(bodyParser.json());
 
 app.use(passport.initialize());
